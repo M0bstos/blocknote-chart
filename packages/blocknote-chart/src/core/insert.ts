4f32d47def55aa3dc5cdfc0a19040b3f1de2ab54
Harsh Nanda <harshnanda47@gmail.com>
2026-03-29 20:04:28 +0530

    standardize insertChart for BlockNote slash menu

--- packages/blocknote-chart/src/core/insert.ts
+++ packages/blocknote-chart/src/core/insert.ts
@@ -1,6 +1,24 @@
-export const insertChart = () => {
+import type { BlockNoteEditor } from "@blocknote/core";
+import { insertOrUpdateBlockForSlashMenu } from "@blocknote/core";
+import type { DefaultSuggestionItem } from "@blocknote/core";
+import { defaultChartData } from "./data";
+
+export const insertChart = (
+  editor: BlockNoteEditor<any, any, any>
+): Omit<DefaultSuggestionItem, "key"> & { key: string } => {
   return {
+    key: "chart",
     title: "Chart",
-    onItemClick: () => {},
+    group: "Media",
+    aliases: ["chart", "graph", "plot", "bar", "line", "pie"],
+    onItemClick: () => {
+      insertOrUpdateBlockForSlashMenu(editor, {
+        type: "chart",
+        props: {
+          chartType: "bar",
+          chartData: JSON.stringify(defaultChartData),
+        },
+      });
+    },
   };
 };
